0d0b1c4572b0b32810c637845545932b4fa4ae2a
Ksoso <k.sosinski@hotmail.com>
2019-11-04 11:48:26 +0100

    change priviliges to user polsl

--- sql/user.sql
+++ sql/user.sql
@@ -1,6 +1,5 @@
 --Tworzymy użytkownika na potrzeby części serwerowej aplikacji
 create role polsl with login password 'polsl_gis';
-alter role polsl CREATEDB;
 
 -- dodajemy uprawnienia do sekwencji, które generują klucze główne
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO polsl;
