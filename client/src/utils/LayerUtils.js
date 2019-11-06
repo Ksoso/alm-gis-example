bf3ef4d3061e69bc42f4ae9fd9d003f6ad297921
Ksoso <k.sosinski@hotmail.com>
2019-11-06 08:23:48 +0100

    change of project name

--- client/src/utils/LayerUtils.js
+++ client/src/utils/LayerUtils.js
@@ -7,45 +7,44 @@ import Cluster from "ol/source/Cluster";
 
 export const LayerUtils = {
 
-        createWMTS: async (url, wmtsParams, appOptions, visible) => {
-            const parser = new WMTSCapabilities();
-            const response = await fetch(`${url}?SERVICE=WMTS&request=GetCapabilities`);
-            const capabilities = await response.text();
+    createWMTS: async (url, wmtsParams, appOptions, visible) => {
+        const parser = new WMTSCapabilities();
+        const response = await fetch(`${url}?SERVICE=WMTS&request=GetCapabilities`);
+        const capabilities = await response.text();
 
-            const optFromCapabilities = optionsFromCapabilities(parser.read(capabilities), wmtsParams);
+        const optFromCapabilities = optionsFromCapabilities(parser.read(capabilities), wmtsParams);
 
-            const tileLayer = new TileLayer({
-                source: new WMTS(optFromCapabilities),
-                visible
-            });
+        const tileLayer = new TileLayer({
+            source: new WMTS(optFromCapabilities),
+            visible
+        });
 
-            tileLayer.setProperties(appOptions, true);
-            return tileLayer;
-        },
+        tileLayer.setProperties(appOptions, true);
+        return tileLayer;
+    },
 
-        createWMS: (url, wmsParams, appOptions) => {
-            const tileLayer = new TileLayer({
-                source: new TileWMS({
-                    url, params: wmsParams
-                })
-            });
+    createWMS: (url, wmsParams, appOptions) => {
+        const tileLayer = new TileLayer({
+            source: new TileWMS({
+                url, params: wmsParams
+            })
+        });
 
-            tileLayer.setProperties(appOptions, true);
-            return tileLayer;
-        },
+        tileLayer.setProperties(appOptions, true);
+        return tileLayer;
+    },
 
-        updateLayer: (layers, type) => {
-            const layerToUpdate = layers.getArray().find(l => type === l.get('type'));
-            if (layerToUpdate) {
-                if (layerToUpdate.getSource() instanceof Cluster) {
-                    layerToUpdate.getSource().getSource().refresh();
-                } else if (layerToUpdate.getSource() instanceof VectorSource) {
-                    layerToUpdate.getSource().refresh();
-                } else {
-                    layerToUpdate.getSource().updateParams({'REVISION': new Date().toISOString()});
-                }
+    updateLayer: (layers, type) => {
+        const layerToUpdate = layers.getArray().find(l => type === l.get('type'));
+        if (layerToUpdate) {
+            if (layerToUpdate.getSource() instanceof Cluster) {
+                layerToUpdate.getSource().getSource().refresh();
+            } else if (layerToUpdate.getSource() instanceof VectorSource) {
+                layerToUpdate.getSource().refresh();
+            } else {
+                layerToUpdate.getSource().updateParams({'REVISION': new Date().toISOString()});
             }
         }
-
     }
-;
+
+};
